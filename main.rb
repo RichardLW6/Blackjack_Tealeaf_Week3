10259edfe87087bf0fe8f6b834a81f20e8f4ff0c
Richard Wilkerson <richard@time2track.com>
2015-08-01 16:29:45 -0400

    Setting up folders and files for Blackjack

---
+++ main.rb
@@ -0,0 +1,7 @@
+require 'rubygems'
+require 'sinatra'
+require 'pry'
+
+use Rack::Session::Cookie, :key => 'rack.session',
+                           :path => '/',
+                           :secret => 'my_lil_string'
