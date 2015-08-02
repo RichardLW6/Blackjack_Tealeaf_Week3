d82f73034cb6a2f44f37638ce2b51102bd1d10d4
Richard Wilkerson <richard@time2track.com>
2015-08-01 20:30:49 -0400

    Added form to get username

--- main.rb
+++ main.rb
@@ -5,3 +5,17 @@ require 'pry'
 use Rack::Session::Cookie, :key => 'rack.session',
                            :path => '/',
                            :secret => 'my_lil_string'
+
+
+
+get '/' do
+  erb :get_name
+end
+
+post '/welcome' do
+  session[:username] = params[:username]
+end
+
+get '/welcome' do
+  erb :welcome
+end
