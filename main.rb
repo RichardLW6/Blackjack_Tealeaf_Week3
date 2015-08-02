db16f18b170071ad39cc4bce19865ab5e5233b4e
Richard Wilkerson <richard@time2track.com>
2015-08-02 14:04:43 -0400

    Player can hit/stay and cards are now images

--- main.rb
+++ main.rb
@@ -6,6 +6,59 @@ use Rack::Session::Cookie, :key => 'rack.session',
                            :path => '/',
                            :secret => 'my_lil_string'
 
+
+helpers do 
+
+  def calculate_total(cards) # cards appear as nested array
+
+    arr = cards.map{|element| element[1]}
+
+    total = 0
+
+    arr.each do |a|
+      if a == "A"
+        total += 11
+      else
+        total += a.to_i == 0 ? 10 : a.to_i
+      end
+    end
+
+    # correct for Aces
+    arr.select {|element| element == "A"}.count.times do
+      break if total <= 21
+      total -= 10
+    end
+
+    total
+  end
+
+  def card_image(card) 
+    suit = case card[0]
+      when 'H' then 'hearts'
+      when 'D' then 'diamonds'
+      when 'C' then 'clubs'
+      when 'S' then 'spades'
+    end
+
+    value = card[1]
+    if ['J', 'Q', 'K', 'A'].include?(value)
+      value = case card[1]
+        when 'J' then 'jack'
+        when 'Q' then 'queen'
+        when 'K' then 'king'
+        when 'A' then 'ace'
+      end
+    end
+
+    "<img src='/images/cards/#{suit}_#{value}.jpg' class='card_image'>"
+  end
+
+end
+
+before do
+  @show_hit_or_stay_buttons = true
+end
+
 get '/' do
   if session[:player_name]
     redirect '/game'
@@ -19,6 +72,11 @@ get '/new_player' do
 end
 
 post '/new_player' do
+  if params[:player_name].empty?
+    @error = "Name is required"
+    halt erb(:new_player)
+  end
+
   session[:player_name] = params[:player_name]
   redirect '/game'
 end
@@ -26,7 +84,7 @@ end
 get '/game' do
   # create a deck and put it in session
   suits = ['H', 'D', 'C', 'S']
-  values = ['2', '3', '4 ', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
   session[:deck] = suits.product(values).shuffle!
 
   # deal cards
@@ -39,3 +97,32 @@ get '/game' do
 
   erb :game
 end
+
+post '/game/player/hit' do
+  session[:player_cards] << session[:deck].pop
+
+  player_total = calculate_total(session[:player_cards])
+  if player_total == 21
+    @success = "Congratulations! #{session[:player_name]} hit blackjack!"
+    @show_hit_or_stay_buttons = false
+  elsif player_total > 21
+    @error = "It looks like #{session[:player_name]} has busted!"
+    @show_hit_or_stay_buttons = false
+  end
+
+  erb :game
+end
+
+post '/game/player/stay' do
+  @success = "#{session[:player_name]} has chosen to stay."
+  @show_hit_or_stay_buttons = false
+  erb :game
+end
+
+
+
+
+
+
+
+
