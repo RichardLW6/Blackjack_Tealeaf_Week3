9a115130bd97ec9af746cfd2ee983b5610d6f2b4
Richard Wilkerson <richard@time2track.com>
2015-08-01 21:14:27 -0400

    Set up basic gameplay for blackjack

--- main.rb
+++ main.rb
@@ -6,16 +6,36 @@ use Rack::Session::Cookie, :key => 'rack.session',
                            :path => '/',
                            :secret => 'my_lil_string'
 
-
-
 get '/' do
-  erb :get_name
+  if session[:player_name]
+    redirect '/game'
+  else
+    redirect '/new_player'
+  end
 end
 
-post '/welcome' do
-  session[:username] = params[:username]
+get '/new_player' do
+  erb :new_player
 end
 
-get '/welcome' do
-  erb :welcome
+post '/new_player' do
+  session[:player_name] = params[:player_name]
+  redirect '/game'
+end
+
+get '/game' do
+  # create a deck and put it in session
+  suits = ['H', 'D', 'C', 'S']
+  values = ['2', '3', '4 ', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  session[:deck] = suits.product(values).shuffle!
+
+  # deal cards
+  session[:dealer_cards] = []
+  session[:player_cards] = []
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+
+  erb :game
 end
